163490ce8fba778b9345126dc24a648a22baa6cc
Tiago Moraes <tiagoefmoraes@gmail.com>
2015-10-28 17:08:18 -0300

    add UserRepository

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'rom', github: 'rom-rb/rom', branch: 'master'
 gem 'rom-repository', github: 'rom-rb/rom-repository', branch: 'master'
 gem 'rom-csv', github: 'rom-rb/rom-csv', branch: 'master'
 gem 'inflecto'
--- run.rb
+++ run.rb
@@ -2,12 +2,23 @@ require 'bundler'
 Bundler.setup
 
 require 'rom-repository'
+require 'rom/plugins/relation/view'
+require 'rom/plugins/relation/key_inference'
 
 env = ROM::Environment.new
 env.setup(:csv, 'users.csv')
 
 class Users < ROM::Relation[:csv]
-  view(:base, [:id, :name]) do
+  dataset :users
+
+  use :view
+  use :key_inference
+
+  def primary_key
+    :id
+  end
+
+  view(:base, [:id, :name, :email]) do
     self
   end
 
@@ -21,3 +32,22 @@ env.register_relation(Users)
 rom = env.finalize.container
 
 p rom.relation(:users).by_id(1).one
+
+
+
+
+
+
+
+class UserRepository < ROM::Repository::Base
+ relations :users
+
+ def get(id)
+   users.by_id(id)
+ end
+
+end
+
+repo = UserRepository.new(rom)
+
+puts repo.get(1)
